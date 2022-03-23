fd9c857b1c097d2f2451f4c3026d2c87e5417c76
bigvo <vladcyoung@gmail.com>
2022-03-24 00:50:10 +0100

    Changed structure to add lib.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ import PackageDescription
 let package = Package(
     name: "swift-quantis-cli",
     products: [
-        .library(name: "SwiftQuantis", targets: ["SwiftQuantisCLI"]),
+        .library(name: "SwiftQuantis", targets: ["SwiftQuantis"]),
         .executable(name:"SwiftQuantisCLI", targets: ["SwiftQuantisCLI"]),
     ],
     dependencies: [
@@ -14,8 +14,12 @@ let package = Package(
     ],
     targets: [
         .target(name: "Quantis", path: "./Sources/Quantis"),
-        .target(name: "SwiftQuantisCLI", dependencies: [
+        .target(name: "SwiftQuantis", dependencies: [
             "Quantis",
+        ]),
+        .executableTarget(name: "SwiftQuantisCLI", dependencies: [
+            "Quantis",
+            "SwiftQuantis",
             .product(name: "ArgumentParser", package: "swift-argument-parser"),
         ]),
     ]
--- Sources/SwiftQuantisCLI/Main.swift
+++ Sources/SwiftQuantisCLI/Main.swift
@@ -8,6 +8,7 @@
 import Foundation
 import ArgumentParser
 import Quantis
+import SwiftQuantis
 
 @main
 struct QuantisCLI: ParsableCommand {
