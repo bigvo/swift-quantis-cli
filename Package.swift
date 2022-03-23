ed1b170c74674c8b75d425567298d775b9f667db
bigvo <vladcyoung@gmail.com>
2022-03-24 00:37:39 +0100

    Package.swift update

--- Package.swift
+++ Package.swift
@@ -6,16 +6,15 @@ import PackageDescription
 let package = Package(
     name: "swift-quantis-cli",
     products: [
-        .library(name: "SwiftQuantis", targets: ["SwiftQuantisCLI"])
+        .library(name: "SwiftQuantis", targets: ["SwiftQuantisCLI"]),
+        .executable(name:"SwiftQuantisCLI", targets: ["SwiftQuantisCLI"]),
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser", from: "1.1.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(name: "Quantis", path: "./Sources/Quantis"),
-        .executableTarget(name: "SwiftQuantisCLI", dependencies: [
+        .target(name: "SwiftQuantisCLI", dependencies: [
             "Quantis",
             .product(name: "ArgumentParser", package: "swift-argument-parser"),
         ]),
